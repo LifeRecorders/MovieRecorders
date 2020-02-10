e9b5760c863b10d7ec71c27c4311122a7b90c10d
tesschung <geobera0910@naver.com>
2020-02-11 08:47:37 +0900

    release

--- movie-back/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'accounts'
-urlpatterns = [
-    path('users/', views.user_detail, name='user_detail'),
-    path('register/', views.user_signup, name='user_signup')
-]
